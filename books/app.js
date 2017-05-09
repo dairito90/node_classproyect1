9555bf1384bb63e11f93982ea041a28068183344
Dairon Rodriguez <drodriguez@fvi.edu>
2017-05-08 22:32:50 -0400

    added books

--- books/app.js
+++ books/app.js
@@ -14,6 +14,54 @@ mongoose.connect(db);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.get('/', function(req,res) {
+    res.send('Happy to be here!!')
+});
+
+
+app.post('/book', function(req,res){
+    var newBook = new Book ();
+    newBook.title = req.body.title;
+    newBook.author = req.body.author;
+    newBook.category = req.body.category;
+
+    newBook.save(function(err, book){
+        if (err) {
+            res.send(err);
+        } else {
+            console.log(book);
+            res.send(book);
+        }
+    });
+});
+
+
+app.get('/books', function(req,res){
+    Book.find({});
+    .exec(function (err, books){
+        if (err) {
+            res.send(err);
+        } else {
+            console.log(books);
+            res.json(books);
+        }
+    });
+});
+
+app.get('/books/:id', function(req,res){
+    Book.findOne({
+        _id:req.params.id
+    })
+    .exec(function(err, books){
+        if (err) {
+            res.send(err);
+        } else {
+            console.log(book);
+            res.json(book);
+        }
+    })
+});
+
 
 
 app.listen(PORT, function(){
